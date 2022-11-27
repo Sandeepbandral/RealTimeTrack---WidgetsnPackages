df35cec77a565aff038ae4931edc2fdd440d0d64
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2022-11-27 12:18:17 +0530

    add dimension

---
+++ lib/src/dimension.dart
@@ -0,0 +1,52 @@
+class Dimension {
+  Dimension._();
+
+  static const _scaleFactor = 4.0;
+  static const d0 = 0.0;
+  static const d0_25 = _scaleFactor * 0.25;
+  static const d0_50 = _scaleFactor * 0.50;
+  static const d0_75 = _scaleFactor * 0.75;
+  static const d1 = _scaleFactor * 1;
+  static const d1_50 = _scaleFactor * 1.50;
+  static const d2 = _scaleFactor * 2;
+  static const d2_50 = _scaleFactor * 2.50;
+  static const d3 = _scaleFactor * 3;
+  static const d4 = _scaleFactor * 4;
+  static const d4_50 = _scaleFactor * 4.50;
+  static const d5 = _scaleFactor * 5;
+  static const d6 = _scaleFactor * 6;
+  static const d7 = _scaleFactor * 7;
+  static const d8 = _scaleFactor * 8;
+  static const d9 = _scaleFactor * 9;
+  static const d10 = _scaleFactor * 10;
+  static const d11 = _scaleFactor * 11;
+  static const d12 = _scaleFactor * 12;
+  static const d13 = _scaleFactor * 13;
+  static const d14 = _scaleFactor * 14;
+  static const d15 = _scaleFactor * 15;
+  static const d16 = _scaleFactor * 16;
+  static const d17 = _scaleFactor * 17;
+  static const d18 = _scaleFactor * 18;
+  static const d19 = _scaleFactor * 19;
+  static const d20 = _scaleFactor * 20;
+  static const d22 = _scaleFactor * 22;
+  static const d24 = _scaleFactor * 24;
+  static const d26 = _scaleFactor * 26;
+  static const d28 = _scaleFactor * 28;
+  static const d30 = _scaleFactor * 30;
+  static const d32 = _scaleFactor * 32;
+  static const d34 = _scaleFactor * 34;
+  static const d36 = _scaleFactor * 36;
+  static const d38 = _scaleFactor * 38;
+  static const d40 = _scaleFactor * 40;
+  static const d44 = _scaleFactor * 44;
+
+  static const d48 = _scaleFactor * 48;
+  static const d58 = _scaleFactor * 58;
+  static const d60 = _scaleFactor * 60;
+  static const d64 = _scaleFactor * 64;
+  static const d68 = _scaleFactor * 68;
+  static const d82 = _scaleFactor * 82;
+  static const d88 = _scaleFactor * 88;
+  static const d90 = _scaleFactor * 90;
+}
