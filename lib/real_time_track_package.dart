88de0ab473deab04da9c2e28adf915e217579b25
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2022-11-27 11:15:46 +0530

    updated

--- lib/real_time_track_package.dart
+++ lib/real_time_track_package.dart
@@ -1,6 +1,7 @@
 library real_time_track_package;
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:real_time_track_package/debug.dart';
 
 export 'src/real_time_track_theme_data.dart';
 export 'src/real_time_track_constants.dart';
@@ -10,4 +11,8 @@ export 'debug.dart';
 
 void googleFontsAllowRuntimeFetching(bool allow) {
   GoogleFonts.config.allowRuntimeFetching = allow;
+}
+
+void enableDebug(bool allow) {
+  Debug.enabled = allow;
 }
