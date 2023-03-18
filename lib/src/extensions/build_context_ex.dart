9a1cb3248c68e738033eb9eb33dbb9d4ac900f1e
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2023-03-18 11:52:38 +0530

    updated

--- lib/src/extensions/build_context_ex.dart
+++ lib/src/extensions/build_context_ex.dart
@@ -9,5 +9,7 @@ extension BuildContextEx on BuildContext {
 
   ThemeData get theme => Theme.of(this);
 
+  TextTheme get textTheme => theme.textTheme;
+
   ColorScheme get colorScheme => theme.colorScheme;
 }
