32f440d3c334950a9403f3aa622baff6789ab954
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2023-05-14 12:34:07 +0530

    updated

---
+++ lib/src/widgets/image_picker_source_sheet.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:real_time_track_package/real_time_track_package.dart';
+
+class ImagePickerSourceSheet extends StatelessWidget {
+  const ImagePickerSourceSheet._();
+
+  static Future<ImageSource?> show(BuildContext context) async {
+    return await showModalBottomSheet<ImageSource?>(
+      context: context,
+      builder: (context) => const ImagePickerSourceSheet._(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScrollableColumn.withSafeArea(
+      isScrollable: false,
+      mainAxisSize: MainAxisSize.min,
+      padding: const EdgeInsets.only(top: 10),
+      children: [
+        _ImageSourceListTile(
+          icon: RttAssets.camera,
+          title: 'Take Phone',
+          source: ImageSource.camera,
+        ),
+        _ImageSourceListTile(
+          icon: RttAssets.picture,
+          title: 'Choose from phone',
+          source: ImageSource.gallery,
+        )
+      ],
+    );
+  }
+}
+
+class _ImageSourceListTile extends StatelessWidget {
+  final String icon;
+  final String title;
+  final ImageSource source;
+
+  const _ImageSourceListTile({
+    required this.icon,
+    required this.title,
+    required this.source,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(icon),
+      title: Text(
+        title,
+        style: context.textTheme.titleLarge,
+      ),
+      onTap: () => Navigator.pop(context, source),
+    );
+  }
+}
