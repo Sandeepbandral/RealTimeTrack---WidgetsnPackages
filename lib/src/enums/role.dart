f03c76e8ed67522bf3de8382ede3b1dd2e3e77a2
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2023-12-12 18:12:37 +0530

    updated

--- lib/src/enums/role.dart
+++ lib/src/enums/role.dart
@@ -1,7 +1,11 @@
 enum Role {
-  unknown,
-  superAdmin,
-  organizationAdmin,
-  headDriver,
-  associateDriver
+  unknown(id: 0),
+  superAdmin(id: 2),
+  organizationAdmin(id: 3),
+  headDriver(id: 4),
+  associateDriver(id: 5);
+
+  final int id;
+
+  const Role({required this.id});
 }
