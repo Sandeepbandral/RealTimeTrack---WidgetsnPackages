b94244b9e84126ca58871f0fa5b33734a7c5030d
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2023-12-15 23:54:12 +0530

    updated

--- lib/src/models/rtt_items.dart
+++ lib/src/models/rtt_items.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class RttMenuItem {
   final String title;
   final String icon;
+  final double? iconSize;
   final VoidCallback onTap;
 
   const RttMenuItem({
     required this.title,
     required this.icon,
     required this.onTap,
+    this.iconSize,
   });
 }
