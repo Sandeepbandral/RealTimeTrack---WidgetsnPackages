87564aad013ffd71334c15ca1a16909cbb93989d
vvvirani <55572605+vvvirani@users.noreply.github.com>
2024-01-29 14:50:37 +0530

    added terms and policy urls

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -5,3 +5,9 @@ const String kNoInternetConnection =
 
 const String kRequestTimeoutMessage =
     'The request timed out. Please try again.';
+
+const String kTermsAndConditionUrl =
+    'http://privacy-policy-deatils.s3-website.ap-south-1.amazonaws.com/';
+
+const String kPrivacyPolicyUrl =
+    'http://privacy-policy-deatils.s3-website.ap-south-1.amazonaws.com/';
