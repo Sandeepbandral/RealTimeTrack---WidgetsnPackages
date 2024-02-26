55440695b98c1ad6c69bcc0fc3030610c10af3d7
vvvirani <55572605+vvvirani@users.noreply.github.com>
2024-02-26 10:33:01 +0530

    added roles names

--- lib/src/enums/role.dart
+++ lib/src/enums/role.dart
@@ -1,12 +1,13 @@
 enum Role {
-  unknown(id: 0),
-  superAdmin(id: 1),
-  organizationAdmin(id: 2),
-  headDriver(id: 3),
-  associateDriver(id: 4),
-  customer(id: 5);
+  unknown(id: 0, name: ''),
+  superAdmin(id: 1, name: 'Super Admin'),
+  organizationAdmin(id: 2, name: 'Organization Admin'),
+  headDriver(id: 3, name: 'Head Driver'),
+  associateDriver(id: 4, name: 'Associate Driver'),
+  customer(id: 5, name: 'Customer');
 
   final int id;
+  final String name;
 
-  const Role({required this.id});
+  const Role({required this.id, required this.name});
 }
